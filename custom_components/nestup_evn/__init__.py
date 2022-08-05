61b3cd5d274014db957f6b5aa5e9d761efdb918b
trvqhuy <88963333+trvqhuy@users.noreply.github.com>
2022-08-05 10:00:24 +0700

    Update __init__.py

--- custom_components/nestup_evn/__init__.py
+++ custom_components/nestup_evn/__init__.py
@@ -8,6 +8,9 @@ from .const import CONF_CUSTOMER_ID, DOMAIN
 
 PLATFORMS: list[str] = ["sensor"]
 
+async def async_setup(hass: HomeAssistant, config: Config):
+    """Set up this integration using YAML is not supported."""
+    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = Component(
@@ -23,3 +26,9 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
