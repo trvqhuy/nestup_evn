e1a690c3b354642743d2b1638f42bb73847ae139
Quang Huy <coolprojects7899@gmail.com>
2022-08-25 12:11:14 +0700

    Fixes bugs

--- custom_components/nestup_evn/__init__.py
+++ custom_components/nestup_evn/__init__.py
@@ -8,13 +8,13 @@ from .const import DOMAIN
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Establish connection with EVN Cloud."""
     hass.data.setdefault(DOMAIN, {}).setdefault(entry.entry_id, {}).update(entry.data)
-    hass.config_entries.async_setup_platforms(entry, "sensor")
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, "sensor")
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
