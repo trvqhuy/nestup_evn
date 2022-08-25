ec1157daa97dbd20880bc0a895112e9266fba716
trvqhuy <coolprojects7899@gmail.com>
2022-08-25 11:04:36 +0700

    Update __init__.py

--- custom_components/nestup_evn/__init__.py
+++ custom_components/nestup_evn/__init__.py
@@ -1,24 +1,20 @@
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-PLATFORMS = [Platform.SENSOR]
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Establish connection with EVN Cloud."""
     hass.data.setdefault(DOMAIN, {}).setdefault(entry.entry_id, {}).update(entry.data)
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, "sensor")
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, "sensor")
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
